b07fefb23796e720a19f2de4a99c512996d9daa4
pinrongc <pinrongc156@gmail.com>
2022-11-09 14:00:20 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/* const button = document.getElementById("btn");
+const text = document.querySelector("#text");
+
+console.log(button);  */
+
+const Domselectors = {
+    button: document.getElementById("btn"),
+    text: document.querySelector("#text"),
+};
+
+console.log(Domselectors);
