bf04718d361721f36cfbe5b7e5320bd37f43d9ae
pinrongc <pinrongc156@gmail.com>
2022-11-10 14:01:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ console.log(button);  */
 const Domselectors = {
     button: document.getElementById("btn"),
     text: document.querySelector("#text"),
+    box: document.querySelector ("big-black-box"),
 };
 
 console.log(Domselectors);
+
+function backgroundAndText (background, text) {
+    background.style.backgroundColor = "red";
+    text.innerHTML = "This is now a big red box";
+}
+
+backgroundAndText(Domselectors.box, Domselectors.text);
